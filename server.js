ab623b1dd508edc581a60f07e45057862eb0d185
Mateusz Karpisiewicz <mateusz.karpisiewicz@gmail.com>
2016-12-07 13:03:00 +0000

    finished app

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 var express = require('express');
+var moment = require('moment');
 
 var app = express();
 
-app.get('/', function(request, response) {
-    response.send('Hello World!');
+app.get('/:date', function(request, response) {
+    var date = request.params.date;
+    if(isNaN(date)) {
+        date = moment(date, 'MMMM D, YYYY');
+    } else {
+        date = moment(date, 'X');
+    }
+    
+    if(date.isValid()) {
+        var object = {
+            unix:  date.format('X'),
+            natural: date.format('MMMM D, YYYY')
+        }
+    } else {
+        var object = {
+            unix: null,
+            natural: null
+        }
+    }
+    response.send(object);
 });
 
+
 app.listen(8080, function() {
-    console.log('Example app listening on port 3000!');
+    console.log('Timestamp microservice app listening on port 3000!');
 });
